de2dc68b8e60214210af3e77c6f585d29789a672
Olivia W <owploppy9@gmail.com>
2016-10-19 22:26:56 -0400

    My fixed calculator!!!!

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner userinput = new Scanner(System.in);
-        System.out.println("Suhhhhhhh dude!");
-        System.out.println("Need some math help huh?");
-        System.out.println("Enter 1st number:");
-        // poop
-        /*
-            poop too
-        */
-        int num1 = userinput.nextInt();
-        System.out.println("Enter operation (+, -, x,or /)");
-        String operation= userinput.next();
-        System.out.println("Enter 2nd number:");
-        int num2 = userinput.nextInt();
-        if (operation.equals("+"))
-            System.out.println("I got u fam, the answer is " + (num1 + num2));
-        else if (operation.equals("-"))
-            System.out.println("I got u fam, the answer is " + (num1 - num2));
-        else if (operation.equals("x"))
-            System.out.println("I got u fam, the answer is " + (num1 * num2));
-        else if (operation.equals("/"))
-            System.out.println("I got u fam, the answer is " + (num1 / num2));
-        else
-            System.out.println("Not gonna happen my dude");
+        // Declare objects
+        Scanner scanner;
+        Integer num1;
+        Integer num2;
+        String operation;
+//        Integer plusSolution;
+//        Integer minusSolution;
+//        Integer timesSolution;
+//        Integer dividedSolution;
+        Integer solution;
+
+        // Read inputs
+        scanner = new Scanner(System.in);
+
+        System.out.print("Enter 1st number: ");
+        num1 = scanner.nextInt();
+
+        System.out.print("Enter 2nd number: ");
+        num2 = scanner.nextInt();
+
+        System.out.print("Enter operation (+, -, *,or /) ");
+        operation = scanner.next();
+
+        System.out.println();
+        solution = 999;
+
+        if (operation.equals("+")) {
+            solution = num1 + num2;
+        }  else if (operation.equals("-")) {
+            solution = num1 - num2;
+        }  else if (operation.equals("*")) {
+             solution = num1 * num2;
+         } else if (operation.equals("/")) {
+             solution = num1 / num2;
+         } else {
+            System.out.println("Error! [" + operation + "] is invalid!");
+            return;
+         }
+
+         System.out.println("I got u fam, the answer is " + solution);
+
     }
 }
